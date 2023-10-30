de6252b4c8f052fdd09c7f9014420c105a45c20d
stanisloe <stashevvsky@gmail.com>
2023-10-30 16:51:19 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,93 @@
+import json
+
+from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import time
+import requests
+
+ADSPOWER_API_BASE_URL = "http://local.adspower.net:50325"
+PROFILES_TO_OPEN = "profiles_to_open.txt"
+
+
+def map_profile_name_to_id():
+    result = {}
+
+    pages_left = True
+    page = 1
+    profiles = []
+    while pages_left:
+        result = {}
+        query = {'page_size': '100', 'page': page}
+        response = requests.get(ADSPOWER_API_BASE_URL + "/api/v1/user/list", query)
+        profiles.extend(response.json()['data']['list'])
+        if len(response.json()['data']['list']) < 100:
+            pages_left = False
+        else:
+            page += 1
+        time.sleep(1)
+
+    for profileInfo in profiles:
+        name = profileInfo['name']
+        id = profileInfo['user_id']
+        result[name] = id
+    return result
+
+
+def connect_to_profile(profile_id):
+    attempts = 3
+    sleep_time = 20
+    while attempts > 0:
+        try:
+            query = {'user_id': profile_id}
+            response = requests.get(ADSPOWER_API_BASE_URL + "/api/v1/browser/start", query).json()
+            print(response)
+            selenium_url = response["data"]["ws"]["selenium"]
+            driver_path = response["data"]["webdriver"]
+            chrome_options = Options()
+            chrome_options.add_experimental_option("debuggerAddress", selenium_url)
+            chrome_options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
+            service = Service(executable_path=driver_path)
+            return webdriver.Chrome(service=service, options=chrome_options)
+        except Exception as e:
+            print("An exception occurred {}".format(e))
+        time.sleep(sleep_time)
+        sleep_time *= 2
+        attempts -= 1
+
+
+def new_tab(driver, url):
+    driver.switch_to.new_window()
+    driver.get(url)
+
+
+def get_profile_names_to_open():
+    profile_names_to_open = []
+    with open(PROFILES_TO_OPEN, 'r') as file:
+        for line in file:
+            profile_names_to_open.append(line.replace("\n", ""))
+    return profile_names_to_open
+
+
+if __name__ == '__main__':
+    profile_name_to_id = map_profile_name_to_id()
+    profile_names_to_open = get_profile_names_to_open()
+
+    profile_name_to_token = {}
+    for profile_name in profile_names_to_open:
+        profile_id = profile_name_to_id.get(profile_name)
+        driver = connect_to_profile(profile_id=profile_id)
+        new_tab(driver, "https://www.memecoin.org/farming")
+        logs = driver.get_log("performance")
+        profile_name_to_token[profile_name] = "Not authenticated"
+        for log in logs:
+            log_str = str(log)
+            if 'https://memefarm-api.memecoin.org/user/info' in log_str and "Bearer" in log_str:
+                token = (json
+                         .loads(log["message"])["message"]["params"]["request"]["headers"]["authorization"]
+                         .replace("Bearer ", ""))
+                profile_name_to_token[profile_name] = token
+        driver.close()
+        time.sleep(1)
+    print(profile_name_to_token)
